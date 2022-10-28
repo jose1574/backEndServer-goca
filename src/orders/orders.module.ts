4eb89d209e7ce11f0cf2584813345710dab07a30
jose1574 <89107861+jose1574@users.noreply.github.com>
2022-10-28 02:03:09 -0400

    se realiza la funcion para calcular el numero de pedido

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OrdersService } from './services/orders.service';
+import { OrdersController } from './controllers/orders.controller';
 
-@Module({})
+@Module({
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
 export class OrdersModule {}
